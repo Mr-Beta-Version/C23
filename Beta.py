91eac88c586d259c599269e21dfa261b496cf00a
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-05-21 22:26:14 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -1,2 +1,11 @@
-from C23 import menu
-menu()
+import os, platform
+os.system('git pull')
+ 
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from C23 import menu
+    menu()
+elif bit == '32bit':
+    from C23arm import menu
+    menu
