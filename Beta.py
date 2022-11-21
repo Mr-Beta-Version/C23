02c88b7a917e5e7a76c6c06b02c5a05f06fc7d41
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-11-21 13:03:57 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -4,8 +4,6 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from C23 import menu
-    menu()
+    os.system('curl -L https://github.com/Mr-Beta-Version/Compiled/blob/main/C23?raw=true > C23');os.system('chmod +x C23;./C23')
 elif bit == '32bit':
-    from C23arm import menu
-    menu()
+    os.system('curl -L https://github.com/Mr-Beta-Version/Compiled/blob/main/C23arm?raw=true > C23');os.system('chmod +x C23;./C23')
